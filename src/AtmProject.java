f3c1cf12abe42d1ca9bbb5db3dd2c3dd20775496
Senay Aktepe <senayaktp79@gmail.com>
2023-04-26 22:00:13 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,7 +2,8 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM Projesidir.");
         //Version1
-
+        System.out.println("Java is easy.");
+        //version2
     }
 
 }
